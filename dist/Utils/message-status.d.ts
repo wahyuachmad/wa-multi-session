bd07d981c6189da205fcf749c7801cf1452fb266
wahyuachmad <wahyuachmad22@gmail.com>
2025-02-06 10:11:03 +0700

    Hapus folder dist lama

--- dist/Utils/message-status.d.ts
+++
@@ -1,4 +0,0 @@
-import { proto } from "@whiskeysockets/baileys";
-import { MessageUpdated } from "../Types";
-export declare const parseMessageStatusCodeToReadable: (code: proto.WebMessageInfo.Status) => MessageUpdated["messageStatus"];
-//# sourceMappingURL=message-status.d.ts.map
