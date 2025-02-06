1f57feccc2960c64ccd8051ea72705796c888881
wahyuachmad <wahyuachmad22@gmail.com>
2025-02-06 10:34:05 +0700

    hapus is bot

--- src/Messaging/index.ts
+++ src/Messaging/index.ts
@@ -17,7 +17,6 @@ export const sendTextMessage = async ({
   sessionId,
   to,
   text = "",
-  is_bot = false,
   isGroup = false,
   ...props
 }: SendMessageTypes): Promise<proto.WebMessageInfo | undefined> => {
@@ -31,7 +30,6 @@ export const sendTextMessage = async ({
       text: text,
     },
     {
-      is_bot: is_bot,
       quoted: props.answering,
     }
   );
